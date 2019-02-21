3e6b7121dd293cd4cad1668599e051f565c85b78
Rohan Bhangui <bhanguirohan@gmail.com>
2019-02-21 07:44:15 -0500

    added partials and added new section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
+// for the home page/ root
+
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
+
+document.addEventListener("DOMContentLoaded", () => {
+	// const titleTextGroup = document.querySelector("#main #text-container");
+	// const title = titleTextGroup.querySelector("#title-text");
+	// const subtitle = titleTextGroup.querySelector("#subtitle-text");
+
+	// setTimeout( () => {
+	// 	title.classList.remove("invisible");
+	// 	subtitle.classList.remove("invisible");
+	// }, 1000);
+
+	// const actionGroup = document.querySelector("#main #action-container");
+	// const action1 = actionGroup.querySelector("#watch-now");
+	// const action2 = actionGroup.querySelector("#listen-now");
+
+	// setTimeout( () => {
+	// 	action1.classList.remove("invisible");
+	// 	action2.classList.remove("invisible");
+	// }, 2000);
+});
